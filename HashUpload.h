797cefbb41d9809e11f10f84a0ce8bdbfbd6c7d6
Paulo Ricardo Lima <120680636+revzera@users.noreply.github.com>
2024-10-14 20:13:15 -0400

    tratando warnings

--- HashUpload.h
+++ HashUpload.h
@@ -1,18 +1,14 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <cstring>
 #include <string>
 #include <vector>
-
-// Definições de constantes
-#define NUM_BUCKETS 100
-#define BLOCK_SIZE 4096 // 4KB por bloco
-#define BLOCKS_PER_BUCKET 10
-
 
 struct BlockHeader {
     int recordCount;
 };
+
 // Estrutura que representa um artigo
 struct Article {
     int id;
@@ -32,9 +28,21 @@ struct Article {
                   << ", Snippet: " << snippet << std::endl;
     }
 };
-#define RECORDS_PER_BLOCK (BLOCK_SIZE / sizeof(Article))
 
+constexpr size_t NUM_BUCKETS = 100;
+constexpr size_t BLOCK_SIZE = 4096; // 4KB por bloco
+constexpr size_t RECORDS_PER_BLOCK = BLOCK_SIZE / sizeof(Article);
+constexpr size_t BLOCKS_PER_BUCKET = 10;
 
+/* Definições de constantes
+
+constexpr usado devido à ambiguidade no cálculo do C++
+Struct Article tem que ser declarada antes do "RECORDS_PER_BLOCK", pois utiliza o Article para calcular o tamanho
+
+#define NUM_BUCKETS 100
+#define BLOCK_SIZE 4096 // 4KB por bloco
+#define BLOCKS_PER_BUCKET 10
+#define RECORDS_PER_BLOCK (BLOCK_SIZE / sizeof(Article))*/
 
 // Funções de manipulação
 int hashFunction(int id);
@@ -46,4 +54,3 @@ std::vector<Article> processarCSV(const std::string& file_path);
 void insertRecord(const Article& article, const std::string& bucket_filename, const std::string& overflow_filename);
 void gravarArtigosComHash(const std::vector<Article>& articles, const std::string& bucket_filename, const std::string& overflow_filename);
 std::vector<Article> readBucket(int bucket, const std::string& bucket_filename, const std::string& overflow_filename);
-
